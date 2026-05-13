5788ba5edf51ff8976a5b7067b03e363b4e637e7
sairevanth13 <kadambalasairevanth@gmail.com>
2026-05-13 22:31:30 +0530

    Port 587 fix

--- ANIME SCOPE/BACKEND/routes/auth.js
+++ ANIME SCOPE/BACKEND/routes/auth.js
@@ -8,8 +8,8 @@ const User = require('../models/User');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
